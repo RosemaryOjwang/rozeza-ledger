75b6d27537bb83ea6de043608a596d498bf58008
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2026-09-09 10:27:44 +0300

    automate company subscription and chart of accounts

--- backend/companies/signals.py
+++ backend/companies/signals.py
@@ -7,6 +7,8 @@ from django.utils import timezone
 from .models import Company
 from subscriptions.models import Subscription
 
+from .services import create_default_accounts
+
 @receiver(post_save, sender=Company)
 def create_company_subscription(sender, instance, created, **kwargs):
     if created:
@@ -16,5 +18,7 @@ def create_company_subscription(sender, instance, created, **kwargs):
             company=instance,
             status=Subscription.Status.TRIAL,
             trial_start=today,
-            trial_end=today + timedelta(days=365),
+            trial_end=today + timedelta(days=120),
         )
+        
+        create_default_accounts(instance)
